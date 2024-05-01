8a1ab7d3b37a37355d2de84380f1512399c874cd
trsaints <thiago.dev2k3@gmail.com>
2024-05-01 08:19:16 -0300

    update: map to ProjectRepository operations

--- trsaints-frontend-api/Controllers/ProjectController.cs
+++ trsaints-frontend-api/Controllers/ProjectController.cs
@@ -1,69 +1,131 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using trsaints_frontend_api.Models;
-using trsaints_frontend_api.Services;
+using trsaints_frontend_api.DTOs;
+using trsaints_frontend_api.Entities;
+using trsaints_frontend_api.Repositories.Interfaces;
 
 namespace trsaints_frontend_api.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class ProjectController: ControllerBase
     {
-        private readonly ILogger<ProjectController> _logger;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IMapper _mapper;
 
-        public ProjectController(ILogger<ProjectController> logger)
+        public ProjectController(IProjectRepository repository, IMapper mapper)
         {
-            _logger = logger;
+            _projectRepository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public ActionResult<List<Project>> GetAll() => ProjectService.GetAll() ?? throw new InvalidOperationException();
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll()
+        {
+            var projects = await _projectRepository.GetAllAsync();
+
+            return Ok(_mapper.Map<IEnumerable<ProjectDTO>>(projects));
+        }
 
         [HttpGet("{id:int}")]
-        public ActionResult<Project> Get(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
         {
-            var project = ProjectService.Get(id);
-
+            var project = await _projectRepository.GetByIdAsync(id);
+            
             if (project is null)
                 return NotFound();
 
-            return project;
+            return Ok(_mapper.Map<ProjectDTO>(project));
         }
 
+        [HttpGet]
+        [Route("get-projects-by-category/{categoryId:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProjectsByStack(int stackId)
+        {
+            var projects = await _projectRepository.GetProjectsByStackAsync(stackId);
+
+            if (!projects.Any())
+                return NotFound();
+            
+            return Ok(_mapper.Map<IEnumerable<ProjectDTO>>(projects));
+        }
+        
         [HttpPost]
-        public IActionResult Create(Project project)
-        {            
-            _logger.LogDebug("Received POST request to create project: {Project}", project);
-            ProjectService.Add(project);
-            return CreatedAtAction(nameof(Get), new { id = project.Id }, project);
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add(ProjectDTO projectDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var project = _mapper.Map<Project>(projectDto);
+            await _projectRepository.AddAsync(project);
+
+            return Ok(_mapper.Map<ProjectDTO>(project));
         }
         
         [HttpPut("{id:int}")]
-        public IActionResult Update(int id, Project project)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, ProjectDTO projectDto)
         {
-            if (id != project.Id)
+            if (id != projectDto.Id)
                 return BadRequest();
-
-            var currentProject = ProjectService.Get(id);
-
-            if (currentProject is null)
-                return NotFound();
             
-            ProjectService.Update(project);
+            if (!ModelState.IsValid)
+                return BadRequest();
+            
+            await _projectRepository.UpdateAsync(_mapper.Map<Project>(projectDto));
 
-            return NoContent();
+            return Ok(projectDto);
         }
 
         [HttpDelete("{id:int}")]
-        public IActionResult Delete(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Remove(int id)
         {
-            var project = ProjectService.Get(id);
-
+            var project = await _projectRepository.GetByIdAsync(id);
+            
             if (project is null)
                 return NotFound();
-            
-            ProjectService.Delete(id);
 
-            return NoContent();
+            await _projectRepository.RemoveAsync(project.Id);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("search/{projectName}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<ProjectDTO>>> Search(string projectName)
+        {
+            var projects = await _projectRepository.SearchAsync(p => p.Name.Contains(projectName));
+
+            if (projects is null)
+                return NotFound();
+
+            return Ok(_mapper.Map<IEnumerable<ProjectDTO>>(projects));
+        }
+
+        [HttpGet]
+        [Route("search-project-with-category/{criteria}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<ProjectDTO>>> SearchProjectWithStack(string criteria)
+        {
+            var projects = _mapper.Map<List<Project>>(await _projectRepository.FindProjectWithStackAsync(criteria));
+
+            if (!projects.Any())
+                return NotFound();
+
+            return Ok(_mapper.Map<IEnumerable<ProjectStackDTO>>(projects));
         }
     }
 }
