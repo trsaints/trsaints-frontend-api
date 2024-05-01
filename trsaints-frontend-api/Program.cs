1efab31c9fc242f35616c94cea00b7c520f6c1da
trsaints <thiago.dev2k3@gmail.com>
2024-05-01 18:56:53 -0300

    feat: add base flow for database implementation

--- trsaints-frontend-api/Program.cs
+++ trsaints-frontend-api/Program.cs
@@ -1,9 +1,32 @@
+using trsaints_frontend_api.Context;
+using trsaints_frontend_api.Mappings;
+using trsaints_frontend_api.Repositories;
+using trsaints_frontend_api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?.Replace("{Host}", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+    .Replace("{Database}", Environment.GetEnvironmentVariable("POSTGRES_DB"))
+    .Replace("{User}", Environment.GetEnvironmentVariable("POSTGRES_USER"))
+    .Replace("{Password}", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString, b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+builder.Services.AddScoped<ITechStackRepository, TechStackRepository>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+
+builder.Services.AddAutoMapper(typeof(DomainToDtoProfile).Assembly);
 
 // Configure logging
 builder.Logging.ClearProviders(); // Remove all other logging providers
