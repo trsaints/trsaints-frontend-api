096fefeb76d294dd6e92f9f8f37bb3518942b394
trsaints <thiago.dev2k3@gmail.com>
2024-05-03 09:15:53 -0300

    update: configure API services for JWT auth

--- trsaints-frontend-api/Program.cs
+++ trsaints-frontend-api/Program.cs
@@ -1,9 +1,14 @@
+using System.Text;
 using trsaints_frontend_api.Context;
 using trsaints_frontend_api.Mappings;
 using trsaints_frontend_api.Repositories;
 using trsaints_frontend_api.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using trsaints_frontend_api.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +34,25 @@ builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 
 builder.Services.AddAutoMapper(typeof(DomainToDtoProfile).Assembly);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"].Replace("{AuthKey}", builder.Configuration.GetValue<string>("JWT_AUTH_KEY"))))
+    };
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
 // Configure logging
 builder.Logging.ClearProviders(); // Remove all other logging providers
 builder.Logging.AddConsole(); // Add console logging
@@ -42,6 +66,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
