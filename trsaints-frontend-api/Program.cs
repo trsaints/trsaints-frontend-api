1f50ea07e915847e9320af7ff688225ae1dd8b84
trsaints <thiago.dev2k3@gmail.com>
2024-05-05 06:25:37 -0300

    update: remove template comments

--- trsaints-frontend-api/Program.cs
+++ trsaints-frontend-api/Program.cs
@@ -5,21 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 Startup.AddCors(builder);
 Startup.AddControllers(builder);
+
 builder.Services.AddEndpointsApiExplorer();
+
 Startup.AddSwagger(builder);
 Startup.AddDbContext(builder);
 Startup.AddScopes(builder);
+
 builder.Services.AddAutoMapper(typeof(DomainToDtoProfile).Assembly);
+
 Startup.AddAuthentication(builder);
 Startup.AddIdentity(builder);
-// Configure logging
-builder.Logging.ClearProviders(); // Remove all other logging providers
-builder.Logging.AddConsole(); // Add console logging
-builder.Logging.SetMinimumLevel(LogLevel.Debug); // Set the minimum log level to Debug
+
+builder.Logging.ClearProviders(); 
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -29,7 +32,5 @@ if (app.Environment.IsDevelopment())
 app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
