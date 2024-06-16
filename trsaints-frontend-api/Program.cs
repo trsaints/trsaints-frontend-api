2cb86f223f8ff03751c5250315a850b6442ed5d9
trsaints <thiago.dev2k3@gmail.com>
2024-06-16 17:11:16 -0300

    feat: implement role-based authorization configurations

--- trsaints-frontend-api/Program.cs
+++ trsaints-frontend-api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using trsaints_frontend_api.Mappings;
 using trsaints_frontend_api;
 using trsaints_frontend_api.Authorization;
@@ -19,11 +20,12 @@ builder.Services.AddAutoMapper(typeof(DomainToDtoProfile).Assembly);
 Startup.AddAuthentication(builder);
 builder.Services.AddAuthorization();
 
+builder.Services.AddSingleton<IAuthorizationHandler, ResourceUsersAuthorizationHandler>();
 builder.Services.AddSingleton<IAuthorizationHandler, ResourceAdministratorsAuthorizationHandler>();
 
 Startup.AddIdentity(builder);
 
-builder.Logging.ClearProviders(); 
+builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
@@ -34,6 +36,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    Startup.EnsureCreatedRoles(roleManager).Wait();
+}
+
+Startup.SeedDb(app);
 
 app.UseCors();
 app.UseAuthentication();
