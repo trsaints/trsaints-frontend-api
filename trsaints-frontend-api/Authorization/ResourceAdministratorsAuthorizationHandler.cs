aad76fdf08436c7fbda0d8ea23760bc6f1580067
trsaints <thiago.dev2k3@gmail.com>
2024-06-16 17:15:36 -0300

    update: add proper operations to AdministratorsRole users

--- trsaints-frontend-api/Authorization/ResourceAdministratorsAuthorizationHandler.cs
+++ trsaints-frontend-api/Authorization/ResourceAdministratorsAuthorizationHandler.cs
@@ -3,9 +3,17 @@ using trsaints_frontend_api.Entities;
 
 namespace trsaints_frontend_api.Authorization;
 
-public class ResourceAdministratorsAuthorizationHandler: ResourceRoleAuthorizationHandler<OperationAuthorizationRequirement, Entity>
+public class ResourceAdministratorsAuthorizationHandler: ResourceRoleAuthorizationHandler<Entity>
 {
-    public ResourceAdministratorsAuthorizationHandler() : base(ResourceOperationConstants.RoleAdministrators)
+    public ResourceAdministratorsAuthorizationHandler()
+        : base(ResourceOperationsConstants.RoleAdministrators, [
+            ResourceOperations.Create,
+            ResourceOperations.Read,
+            ResourceOperations.Update,
+            ResourceOperations.Delete
+        ])
     {
     }
+    
+    
 }
