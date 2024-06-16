0c7e94e6af58162c08742a07531fcedc8e5414b9
trsaints <thiago.dev2k3@gmail.com>
2024-06-16 17:14:04 -0300

    feat: verify actual roles/operations injected by ResourceRoleAuthorizationHandler instances

--- trsaints-frontend-api/Authorization/ResourceRoleAuthorizationHandler.cs
+++ trsaints-frontend-api/Authorization/ResourceRoleAuthorizationHandler.cs
@@ -1,23 +1,29 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace trsaints_frontend_api.Authorization;
 
-public class ResourceRoleAuthorizationHandler<TRequirement, TResource>
-    : AuthorizationHandler<TRequirement, TResource> where TRequirement: IAuthorizationRequirement
+public class ResourceRoleAuthorizationHandler<TResource>
+    : AuthorizationHandler<OperationAuthorizationRequirement, TResource> 
 {
     private readonly string _role;
+    private readonly OperationAuthorizationRequirement[] _operations;
     
-    public ResourceRoleAuthorizationHandler(string role)
+    public ResourceRoleAuthorizationHandler(string role, OperationAuthorizationRequirement[] operations)
     {
         _role = role;
+        _operations = operations;
     }
     
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, TRequirement requirement, TResource resource)
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement, TResource resource)
     {
         if (context.User is null)
             return Task.CompletedTask;
 
-        if (context.User.IsInRole(_role)) 
+        var operationIsAllowed = _operations.Contains(requirement);
+        var hasRole = context.User.IsInRole(_role);
+        
+        if (hasRole && operationIsAllowed)
             context.Succeed(requirement);
         
         return Task.CompletedTask;
