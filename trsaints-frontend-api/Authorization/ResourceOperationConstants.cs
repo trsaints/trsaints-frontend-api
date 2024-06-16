cda2ee4d16cdbba0debeb6b1d87b54ffdf44dd16
trsaints <thiago.dev2k3@gmail.com>
2024-06-16 17:12:37 -0300

    feat: add AuthorizationRequirement operations and roles

--- trsaints-frontend-api/Authorization/ResourceOperationConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace trsaints_frontend_api.Authorization;
-
-public class ResourceOperationConstants
-{
-    public const string RoleAdministrators = "Administrator";
-    public const string RoleUsers = "User";
-}
