3be7c2db93ad2570c26812ef502f555427ca055a
trsaints <thiago.dev2k3@gmail.com>
2024-06-16 17:07:50 -0300

    feat: create dependency injection controller for handling authorization

---
+++ trsaints-frontend-api/Controllers/DI_BaseController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using trsaints_frontend_api.Context;
+using trsaints_frontend_api.Entities;
+
+namespace trsaints_frontend_api.Controllers;
+
+public class DI_BaseController: ControllerBase
+{
+    protected AppDbContext Context { get; }
+    protected IAuthorizationService AuthorizationService { get; }
+    protected UserManager<ApplicationUser> UserManager { get; }
+
+    public DI_BaseController(
+        AppDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<ApplicationUser> userManager)
+    {
+        Context = context;
+        UserManager = userManager;
+        AuthorizationService = authorizationService;
+    }
+}
