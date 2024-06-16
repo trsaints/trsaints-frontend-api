285a5ae89cdef6507a682c73dfcd20ca2e7aa402
trsaints <thiago.dev2k3@gmail.com>
2024-06-15 23:44:48 -0300

    feat: add constants for manipulating IdentityRole instances

---
+++ trsaints-frontend-api/Authorization/ResourceOperationConstants.cs
@@ -0,0 +1,7 @@
+namespace trsaints_frontend_api.Authorization;
+
+public class ResourceOperationConstants
+{
+    public const string RoleAdministrators = "Administrator";
+    public const string RoleUsers = "User";
+}
