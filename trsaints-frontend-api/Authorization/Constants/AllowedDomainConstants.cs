41504e5d54cf333ff9da5db981783b495fbd8e49
trsaints <thiago.dev2k3@gmail.com>
2024-06-20 20:49:35 -0300

    update: separate cors domains from application hosts

--- trsaints-frontend-api/Authorization/Constants/AllowedDomainConstants.cs
+++ trsaints-frontend-api/Authorization/Constants/AllowedDomainConstants.cs
@@ -3,5 +3,6 @@ namespace trsaints_frontend_api.Authorization.Constants;
 public static class AllowedDomainConstants
 {
     public const string DomainPolicy = "AllowSpecificOrigins";
-    public const string DomainNames = "ALLOWED_DOMAINS";
+    public const string DomainHostNames = "ALLOWED_DOMAINS";
+    public const string DomainCorsHostNames = "CORS_DOMAINS";
 }
