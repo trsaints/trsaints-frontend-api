2994a6f7e9b71b9d42756495f06bd7f04e311b0d
trsaints <thiago.dev2k3@gmail.com>
2024-08-17 11:03:01 -0300

    update: rename environment variable name

--- trsaints-frontend-api/Constants/AllowedDomainConstants.cs
+++ trsaints-frontend-api/Constants/AllowedDomainConstants.cs
@@ -4,5 +4,5 @@ public static class AllowedDomainConstants
 {
     public const string DomainPolicy = "AllowSpecificOrigins";
     public const string DomainHostNames = "ALLOWED_DOMAINS";
-    public const string DomainCorsHostNames = "CORS_DOMAINS";
+    public const string DomainCorsHostNames = "ALLOWED_CORS_DOMAINS";
 }
